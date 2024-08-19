731f33e289feea31e878b36fd4b67b45e23990b4
Omer Yanai <omery33111@gmail.com>
2024-08-19 18:21:42 +0300

    SOCKET

--- front-end/src/features/base/MainPage.tsx
+++ front-end/src/features/base/MainPage.tsx
@@ -38,7 +38,7 @@ const MainPage = () => {
 
 
     useEffect(() => {
-      const socketInstance = new WebSocket('wss://jammoveo-backend.onrender.com/ws/chat/');
+      const socketInstance = new WebSocket('wss://jamoveo-backend.onrender.com/ws/chat/');
 
         socketInstance.onopen = () => {
             console.log('WebSocket is open now.');
--- front-end/src/features/base/ResultsPage.tsx
+++ front-end/src/features/base/ResultsPage.tsx
@@ -15,7 +15,7 @@ const ResultsPage = () => {
 
 
     useEffect(() => {
-        const socketInstance = new WebSocket('wss://jammoveo-backend.onrender.com/ws/chat/');
+        const socketInstance = new WebSocket('wss://jamoveo-backend.onrender.com/ws/chat/');
         setSocket(socketInstance);
         
         socketInstance.onopen = () => {
