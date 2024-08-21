42c1e84dd4fee1e5bce0c15276a150bdf1f2e966
Omer Yanai <omery33111@gmail.com>
2024-08-21 04:15:18 +0300

    Finishes

--- front-end/src/endpoints/endpoints.ts
+++ front-end/src/endpoints/endpoints.ts
@@ -4,7 +4,7 @@
 
 
 // ------------------------- URL_BACKEND ENDPOINTS:
-export const URL_BACKEND = process.env.REACT_APP_BACKEND_URL || "http://127.0.0.1:8000";
+export const URL_BACKEND = process.env.REACT_APP_BACKEND_HTTPS_URL || "http://127.0.0.1:8000";
 
 
 
